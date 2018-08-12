b01d62d11e32d7a77322cc4c8028d9afe1df0809
Robert Hutter <rohu747@gmail.com>
2018-08-12 07:18:54 +0200

    Added new class AccelGyro to development framework

---
+++ C++/Arduino/StandardRoboticsLibrary/src/AccelGyro.h
@@ -0,0 +1,59 @@
+/*
+* MIT License
+*
+* Copyright (c) 2018 Robert Hutter
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#ifndef _ACCELGYRO_H
+#define _ACCELGYRO_H
+
+#include <SRL.h>
+#include <Component.h>
+#include <Accelerometer.h>
+#include <Gyroscope.h>
+
+namespace SRL
+{
+  class AccelGyro : public SRL::Accelerometer, public SRL::Gyroscope, public virtual SRL::Component
+  {
+    public:
+      AccelGyro(float aC, float gC);
+
+      virtual int16_t getRawAccelX(void) = 0;
+      virtual int16_t getRawAccelY(void) = 0;
+      virtual int16_t getRawAccelZ(void) = 0;
+
+      virtual int16_t getRawGyroX(void) = 0;
+      virtual int16_t getRawGyroY(void) = 0;
+      virtual int16_t getRawGyroZ(void) = 0;
+
+      /* Getters & setters */
+      float getAccelCoeff(void);
+			void setAccelCoeff(float aC);
+			float getGyroCoeff(void);
+			void setGyroCoeff(float gC);
+
+    protected:
+      float aC;
+      float gC;
+  };
+}
+
+#endif
