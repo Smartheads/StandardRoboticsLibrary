e5ea8e1ca9876ac7d7cd5a4037581ec5b2313ba2
Robert Hutter <rohu747@gmail.com>
2018-08-12 07:18:25 +0200

    Added support for motor with encoder JGY370

---
+++ C++/Arduino/StandardRoboticsLibrary/src/JGY370.h
@@ -0,0 +1,47 @@
+/*
+* MIT License
+*
+* Copyright (c) 2018 Robert Hutter
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#ifndef _JGY370
+#define _JGY370
+
+#include <SRL.h>
+#include <Encoder.h>
+#include <Motor.h>
+
+#define JGY370_COMPONENT_NAME "JGY370"
+#define STEP_IN_CM 0.00051
+#define CM_IN_STEPS 1176.470588
+
+namespace SRL
+{
+  class JGY370 : public SRL::Encoder, public SRL::Motor
+  {
+    public:
+      JGY370(unsigned int forwardPin, unsigned int backwardPin, unsigned int pwmPin, int c1, unsigned int c2);
+
+      long convertSteps(double cm);
+      double convertCm(long steps);
+  };
+}
+
+#endif
