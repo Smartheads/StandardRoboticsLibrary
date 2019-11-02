ee14cada96a60e7b171e5ef98281962dead494d7
deaxuser <rohu747@gmail.com>
2019-11-02 17:41:10 +0100

    Added SCOM to library.

---
+++ C++/Arduino/StandardRoboticsLibrary/src/Slave/Slave.cpp
@@ -0,0 +1,129 @@
+/*
+* MIT License
+*
+* Copyright (c) 2019 Robert Hutter
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#include <Slave.h>
+
+/**
+*	Constructor for the SCOM slave class.
+*
+*/
+SRL::Slave::Slave()
+{
+
+}
+
+/**
+*	Destructor for the SCOM slave class.
+*
+*/
+SRL::Slave::~Slave()
+{
+
+}
+
+/**
+*	Open the SCOM communication. Waits for the master.
+*
+*	@param baudRate The serial connection's baud rate.
+*/
+uint8_t SRL::Slave::openSCOM(unsigned int baudRate)
+{
+	Serial.begin(baudRate);
+	status = WAITING_FOR_MASTER_SIGNAL;
+	
+	// Wait for SOH signal
+	waitForUpdate();
+	
+	// Get version number
+	status = WAITING_FOR_MASTER_SIGNAL;
+	waitForUpdate();
+	
+	// Send version number
+	sendInt16(VERSION);
+	awaitedSum = calculateSum(VERSION);
+	
+	// Wait for responce
+	status = WAITING_FOR_MASTER_RESPONCE;
+	waitForUpdate();
+	
+	// Get version compatibility information
+	status = WAITING_FOR_MASTER_SIGNAL;
+	waitForUpdate();
+	
+	return lastMasterSignal == ETB;
+}
+
+/**
+*	Waits for an update from the master.
+*
+*/
+void SRL::Slave::waitForUpdate(void)
+{
+	while(status != OK_CONTINUE)
+	{
+		updateSCOM();
+	}
+}
+
+/**
+*	Updates the SCOM communication by reading serial input.
+*
+*
+*/
+void SRL::Slave::updateSCOM(void)
+{
+	// Wait for a complete message to arrive
+	if (Serial.available() > 1)
+	{
+		if (status == WAITING_FOR_MASTER_SIGNAL)
+		{
+			// Read an int16_t
+			lastMasterSignal = readInt16();
+			
+			status = WAITING_FOR_MASTER_RESPONCE;
+			
+			sendInt16(calculateSum(lastMasterSignal));
+		}
+		else if (status == WAITING_FOR_MASTER_RESPONCE)
+		{
+			int16_t masterResponce = readInt16();
+			
+			if (masterResponce == ACK)
+			{
+				status = OK_CONTINUE;
+				return;
+			}
+			
+			if (masterResponce == awaitedSum)
+			{
+				sendInt16(ACK);
+				status = OK_CONTINUE;
+				return;
+			}
+			else
+			{
+				// TODO: Implement
+			}
+		}
+	}
+}
